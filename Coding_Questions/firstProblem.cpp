1bca51faaaadd7b0cf7e69106d4e684d07a1a596
ameya-6964 <belvalkar7@gmail.com>
2023-08-21 16:01:05 +0530

    Added Basic Problem To Add Two Numbers

---
+++ Coding_Questions/firstProblem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout << "Enter First Number" << endl; 
+    cin >> a;
+    cout << "Enter Second Number" << endl; 
+    cin >> b;
+    int sum=a+b;
+    cout << sum << endl;
+    return 0;
+}
